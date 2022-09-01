ce1e27e319c2943565676d7cb0942f3300520db1
shaheer ch <shaheer.chry@gmail.com>
2022-09-01 14:55:18 +0500

    edit

--- queries.sql
+++ queries.sql
@@ -159,21 +159,6 @@ SELECT species.name AS specialization,count(*) AS number_of_visits FROM species 
 
 EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animals_id = 4;
 
- 
-
 EXPLAIN ANALYZE SELECT * FROM visits where vets_id = 2;
 
- 
-
 EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
-
- 
-
-
-
-
-
-
-
-
-
